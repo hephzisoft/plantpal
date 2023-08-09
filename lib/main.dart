dfad7bca896f46cf04ac5bea423dbf3ed6bc1999
Hephzisoft <hephzisoft@gmail.com>
2023-08-09 14:28:03 +0100

    Editing the theme of the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/': (ctx) => const HomeScreen(),
       },
