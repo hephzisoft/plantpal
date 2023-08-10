df62038f1cfc3e3155acced6500650fae10def53
Hephzisoft <hephzisoft@gmail.com>
2023-08-10 21:28:34 +0100

    Made some changes to the auth folers

---
+++ lib/screens/verify_email/verify_email_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('verify email Screen')),
+    );
+  }
+}
