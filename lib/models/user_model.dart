bbcba3f5cfe3b8c100e103717616f38be8005a53
Hephzisoft <hephzisoft@gmail.com>
2023-08-10 12:48:51 +0100

    Starting the email authentication

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,15 +1,15 @@
 class User {
   final String uid;
-  final String fullName;
-  final String address;
+  final String? fullName;
+  final String? address;
   final String email;
   final String? age;
   final String? gender;
 
   User({
     required this.uid,
-    required this.fullName,
-    required this.address,
+    this.fullName,
+    this.address,
     required this.email,
     this.age,
     this.gender,
